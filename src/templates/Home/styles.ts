7b974f5b75795ddb19f0a12983ec3ee2966f60e7
Junior Alves <jjalves96@gmail.com>
2021-12-21 07:34:59 -0300

    feat: create Home page component

---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  img {
+    width: 100%;
+    height: 100%;
+
+    object-fit: cover;
+  }
+`;
