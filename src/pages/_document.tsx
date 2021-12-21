2cbf8e7b4c94ecf0608f8c97b140f1939ef0667f
Junior Alves <jjalves96@gmail.com>
2021-12-21 07:33:56 -0300

    chore: add fonts imports

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
