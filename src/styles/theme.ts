85156285ba9bec0815a365d7f8e132996809cc8e
Junior Alves <jjalves96@gmail.com>
2021-12-21 07:34:16 -0300

    style: add new props

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,7 +11,7 @@ export default {
   },
   font: {
     family:
-      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     light: 300,
     normal: 400,
     bold: 600,
