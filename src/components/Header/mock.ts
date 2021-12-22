4ec6c729bac1ffe916a29785463d0a399a3b32dc
Junior Alves <jjalves96@gmail.com>
2021-12-22 07:28:40 -0300

    chore: create mock for Header component

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,19 @@
+import { v4 } from 'uuid';
+
+export const HeaderMock = [
+  {
+    id: v4(),
+    name: 'Introduction',
+    href: '/'
+  },
+  {
+    id: v4(),
+    name: 'Customization',
+    href: '/'
+  },
+  {
+    id: v4(),
+    name: 'Look inside',
+    href: '/'
+  }
+];
